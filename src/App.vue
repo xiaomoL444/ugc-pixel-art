5b39accbbf2ab671ebe4ba8e80421ea1f92148b1
xiaomoL444 <2802273114@qq.com>
2025-12-24 20:19:15 +0800

    完成基本功能

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,82 @@
 <template>
   <Toaster position="top-center" />
-  <div class="container">
-    <input type="file" accept="image/*" @change="onFileChange" />
+  <div style="display: flex; flex-direction: column;height:100vh;">
+    <div style="/* text-align: center; */
+  display: flex;
+  /* justify-content: center; */
+  /* 水平居中 */
+  align-items: center;
+    flex: 1.2;
+  padding-left: 2rem;   background: linear-gradient(90deg, #6a5acd, #00bfff);">
+      <img :src="imgSrc" style="height: 2.5rem; width: 2.5rem; margin-right: 1rem;">
+      <h2 class="title">像素画生成器</h2>
+    </div>
+    <div class="container">
+      <div class="block" style="display: flex;flex-direction: column; flex:0;  width: 20rem;      /* 固定宽度 */
+  flex-shrink: 0;    /* 不允许被压缩 */">
+        <Title :title="'选择图片'"></Title>
 
-    高度：<input v-model="pixelHeight">
-    宽度：<input v-model="pixelWidth">
-    每行最大像素宽：（建议不超过30）<input v-model="maxPixelWidth">
-    <!-- 原图显示 -->
-    原图：<img v-if="imgUrl" :src="imgUrl" class="preview" />
+        <div style=" margin: 0.25rem; display: flex;
+  flex-direction: column;
+  gap: 12px; /* 子元素上下间距 */">
+          <input ref="fileInput" type="file" style="display: none" accept="image/*" @change="onFileChange" />
 
-    <!-- 像素画 canvas -->
-    预览：<canvas ref="canvas" :width="pixelWidth" :height="pixelHeight"></canvas>
+          <!-- 原图显示 -->
+          <!-- 原图：<img v-if="imgUrl" :src="imgUrl" class="preview" /> -->
 
-    <!-- <pre class="output">
+          <!-- 像素画 canvas -->
+          预览：
+          <div style="overflow-y: auto;height: 16rem;">
+            <canvas
+              :style="{  height: 15.5 * pixelHeight / pixelWidth + 'rem', width: '15.5rem' }"
+              ref="canvas" :width="pixelWidth" :height="pixelHeight"></canvas>
+          </div>
+
+          <button class="selectBtn" @click="selectFile">选择文件</button>
+
+          <div class="ElementBox">
+            <div class="ElementBoxTitle">高度</div>
+            <div class="ElementContent"><input class="input" type="number" v-model="pixelHeight"></div>
+          </div>
+
+
+
+          <div class="ElementBox">
+            <div class="ElementBoxTitle">宽度</div>
+            <div class="ElementContent"><input class="input" type="number" v-model="pixelWidth"></div>
+          </div>
+
+          <div class="ElementBox">
+            <div class="ElementBoxTitle">每行最大像素宽：（建议不超过30）</div>
+            <div class="ElementContent"> <input class="input" type="number" v-model="maxPixelWidth"></div>
+          </div>
+        </div>
+      </div>
+      <div class="block" style="  overflow-y: scroll;flex: 1;">
+        <Title :title="'分段输出'"></Title>
+        <div class="outputContainer" v-for="(line, index) in pixels" :key="index">
+          <CopyBox title='我是标题' :content="line"></CopyBox>
+        </div>
+      </div>
+      <!-- <pre class="output">
 {{ pixels }}
     </pre> -->
-
-    <div v-for="(line, index) in pixels" :key="index">
-      <CopyBox :content="line"></CopyBox>
     </div>
-
   </div>
 </template>
 
 <script setup>
+
+/* eslint-disable */
+
+import logo from '@/assets/7.png'  // @ = src/
+const imgSrc = logo
+
 import { ref } from 'vue'
 import CopyBox from './components/CopyBox.vue'
 import { Toaster } from 'vue-sonner'
 import 'vue-sonner/style.css'
+import Title from './components/TitleBox.vue'
 
 const canvas = ref(null)
 const imgUrl = ref(null)
@@ -35,6 +84,11 @@ const pixelHeight = ref(20)//像素画高
 const pixelWidth = ref(20) //像素画宽
 const maxPixelWidth = ref(20) //每行最大像素画宽
 const pixels = ref([])
+
+const fileInput = ref(null)
+function selectFile() {
+  fileInput.value.click()
+}
 
 function onFileChange(e) {
   const file = e.target.files[0]
@@ -45,6 +99,7 @@ function onFileChange(e) {
   const img = new Image()
   img.onload = () => drawToPixelCanvas(img)
   img.src = imgUrl.value
+  e.target.value = ''
 }
 
 function drawToPixelCanvas(img) {
@@ -54,13 +109,13 @@ function drawToPixelCanvas(img) {
   ctx.imageSmoothingEnabled = false
 
   // 清空画布
-  ctx.clearRect(0, 0, pixelHeight.value, pixelWidth.value)
+  ctx.clearRect(0, 0, pixelWidth.value, pixelHeight.value)
 
   // 把原图压缩绘制到小 canvas
-  ctx.drawImage(img, 0, 0, pixelHeight.value, pixelWidth.value)
+  ctx.drawImage(img, 0, 0, pixelWidth.value, pixelHeight.value)
 
   // 读取像素数据
-  const imageData = ctx.getImageData(0, 0, pixelHeight.value, pixelWidth.value)
+  const imageData = ctx.getImageData(0, 0, pixelWidth.value, pixelHeight.value)
   const data = imageData.data
 
   const result = []
@@ -132,10 +187,62 @@ function rgbaToHex(r, g, b, a = 255) {
 </script>
 
 <style scoped>
+.selectBtn {
+  color: white;
+  background-color: #4FACFE;
+  border: #000;
+  border-radius: 10px;
+  font-size: 1.2rem;
+  padding: 1rem;
+  cursor: pointer;
+  transition:
+    background-color 0.3s ease,
+    transform 0.2s ease-out,
+    color 0.3s ease;
+}
+
+.selectBtn:hover {
+  background-color: #337ecc;
+  /* 更深一点的颜色 */
+
+}
+
+.title {
+  font-weight: 700;
+  /* 粗体 */
+  font-size: 2rem;
+  /* 放大一点好看 */
+  /* background: linear-gradient(0deg, #6a5acd, #00bfff); */
+  /* 紫 → 蓝 */
+  background-color: white;
+  -webkit-background-clip: text;
+  /* 背景裁剪到文字 */
+  color: transparent;
+  /* 隐藏文字颜色，让渐变显现 */
+  -webkit-text-fill-color: transparent;
+  /* Safari 兼容 */
+}
+
 .container {
   display: flex;
-  flex-direction: column;
-  gap: 12px;
+  flex-direction: row;
+  /* gap: 12px; */
+  flex: 18;
+  height: 90vh;
+}
+
+.container .block {
+  border: 1px solid #ccc;
+  flex: 1;
+
+}
+
+.container .block .titleBlock {
+  border: 1px solid #ccc;
+}
+
+.container .block .containerBlock {
+  border: 1px solid #ccc;
 }
 
 .preview {
@@ -157,5 +264,9 @@ canvas {
   color: #0f0;
   padding: 8px;
   font-size: 12px;
+}
+
+.outputContainer {
+  margin: 0.5rem .5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/global.css'  // ← 全局 CSS 文件
 
 createApp(App).mount('#app')
